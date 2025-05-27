5ede2c00d8954e6c5adb1f6b3ccc46b53dc94b3e
Tony Berglund <tony_berglund@outlook.com>
2025-05-27 20:40:59 +0200

    feat: Implement Create Training Program and Calendar functionality with responsive design

---
+++ app/components/Minute_Slider/MinuteSlider.jsx
@@ -0,0 +1,37 @@
+"use client";
+import { useState } from "react";
+import styles from "./MinuteSlider.module.css";
+
+export default function MinuteSlider() {
+    const [value, setValue] = useState(30);
+    const min = 0;
+    const max = 90;
+    const percent = ((value - min) / (max - min)) * 100;
+
+    const sliderStyle = {
+      background: `linear-gradient(to right, #8468ea 0%, #8468ea ${percent}%, #ddd ${percent}%, #ddd 100%)`,
+    };
+
+  return (
+    <div className={styles.sliderContainer}>
+      <label htmlFor="daysRange" className={styles.daysRange}>
+        Välj dagar - {value}
+      </label>
+      <input
+        type="range"
+        id="daysRange"
+        min="0"
+        max="90"
+        value={value}
+        step="1"
+        onChange={(e) => setValue(Number(e.target.value))}
+        className={styles.slider}
+        style={sliderStyle}
+      />
+      <div className={styles.sliderValueContainer}>
+        <span className={styles.sliderValueMin}>1</span>
+        <span className={styles.sliderValueMax}>90</span>
+      </div>
+    </div>
+  );
+}
---
+++ app/create-training-calendar/page.jsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Header_DisplayButton from "@/components/Header_DisplayButton";
+import Calendar from "@/components/Calendar/Calendar";
+import Image from "next/image";
+import imageUrl from "@/assets/traningsprogram.png";
+import styles from "./page.module.css";
+
+export default function CreateTrainingProgramPlan() {
+  const router = useRouter();
+  const [active, setActive] = useState("Kalender");
+
+  const exercise = {
+    name: "Träningsvideo",
+    videoUrl: "https://www.youtube.com/embed/0xcutfMELrk?autoplay=1&mute=1", // Lägg till eller ta bort för att testa fallback
+    imageUrl: "/traningsprogram.png",
+  };
+
+  // const exercise = {
+  //   name: "Armhävningar",
+  //   videoUrl: "", 
+  //   imageUrl: "",
+  // };
+
+  return (
+    <div className={styles.traingProgramContainer}>
+      <header className={styles.header}>
+        <button
+          className={styles.backBtn}
+          onClick={() => router.push("/")}
+        ></button>
+        <h1 className={styles.title}>Skapa träningsprogram</h1>
+      </header>
+      <main className={styles.main}>
+        {exercise.videoUrl &&
+        (exercise.videoUrl.includes("youtube.com") ||
+          exercise.videoUrl.includes("youtu.be")) ? (
+          <div className={styles.videoWrapper}>
+            <iframe
+              src={exercise.videoUrl}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        ) : (
+          <Image
+            src={exercise.imageUrl || imageUrl}
+            alt="Fallback image"
+            width={500}
+            height={500}
+            className={styles.fallbackImage}
+          ></Image>
+        )}
+        <div className={styles.buttonContainer}>
+          <button className={styles.restartButton}>Välj video</button>
+          <Header_DisplayButton
+            links={["Kalender", "Beskrivning"]}
+            onChange={(val) => setActive(val)}
+          />
+        </div>
+        {active === "Kalender" ? (
+          <section className={styles.calendar}>
+            <Calendar />
+          </section>
+        ) : (
+          <section className={styles.description}>
+            <h4 className={styles.title}>Title or description</h4>
+            <p>
+              Free text that works according to MD? So we can write both titles
+              and text and ul lists whatever the user wants.
+            </p>
+          </section>
+        )}
+      </main>
+    </div>
+  );
+}
